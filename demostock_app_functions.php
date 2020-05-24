fd77227c58bdaa11fcf87ed3975549cadcd683d0
vickysw <vanrajkalsariya1992@gmail.com>
2020-05-24 11:31:18 +0530

    Optimise code

--- demostock_app_functions.php
+++ demostock_app_functions.php
@@ -50,6 +50,7 @@ http://localhost/demostock-api/demostock_app.php?action=search
 				$paramQuery = "SELECT * FROM customer_price_quote_parameters WHERE (1=1)";
 				$where = "";
 				$cond  = " AND ";
+				$customerData = [];
 				if($data['shape'] != "")
 				{
 					$shapeData = explode(',', $data['shape']);
@@ -57,7 +58,12 @@ http://localhost/demostock-api/demostock_app.php?action=search
 						$where .= $cond." (".$shape."  BETWEEN dp_shape_from AND dp_shape_to )";
 						$cond = " OR ";
 					}
-					
+
+					// $shape_result = $this->qry($paramQuery . $where,3);
+					if($this->qry($paramQuery . $where,3) > 0 )
+							$customerData['shape'] = $data['shape'];
+					$where = "";
+					$cond  = " AND ";
 				}
 				if($data['color'] != "")
 				{
@@ -66,6 +72,11 @@ http://localhost/demostock-api/demostock_app.php?action=search
 						$where .= $cond." (".$color."  BETWEEN dp_color_from AND dp_color_to )";
 						$cond = " OR ";
 					}
+					// $color_result = $this->qry($paramQuery . $where,3);
+					if($this->qry($paramQuery . $where,3) > 0 )
+							$customerData['color'] = $data['color'];
+					$where = "";
+					$cond  = " AND ";
 				}
 				// if($data['size'] != "")
 				// {
@@ -79,6 +90,12 @@ http://localhost/demostock-api/demostock_app.php?action=search
 						$where .= $cond." (".$clarity."  BETWEEN dp_clarity_from AND dp_clarity_to )";
 						$cond = " OR ";
 					}
+
+					// $clarity_result = $this->qry($paramQuery . $where,3);
+					if($this->qry($paramQuery . $where,3) > 0 )
+							$customerData['clarity'] = $data['clarity'];
+					$where = "";
+					$cond  = " AND ";
 			
 				}
 				if($data['discount'] != "")
@@ -89,55 +106,72 @@ http://localhost/demostock-api/demostock_app.php?action=search
 						$where .= $cond." (".$discount."  BETWEEN dp_discount_from AND dp_discount_to )";
 						$cond = " OR ";
 					}
+
+					// $discount_result = $this->qry($paramQuery . $where,3);
+					if($this->qry($paramQuery . $where,3) > 0 )
+							$customerData['discount'] = $data['discount'];
+					$where = "";
+					$cond  = " AND ";
 				}
 
-				 echo $paramQuery . $where; 
-				$resources = $this->qry($paramQuery . $where,2);
-					echo '<pre>'; print_r($resources); die;
-				$arrangeArray = array_column($resources, 'dp_parameters_code');
+				 if(!empty($customerData)){
+				 	$return_arr = $this->getDiamondData($customerData);
+				 }else
+				 {
+					$return_arr['flag'] = true;
+					$return_arr['data'] = [];
+				 }
 			}else{
-				$paramQuery = "SELECT dp_parameters_code FROM diamond_parameters WHERE ( 1 = 1 )";
-				$where = "";
-				$cond  = " AND ";
-				if($data['shape'] != "")
-				{
-					$where .= $cond." (dp_table_type = 0 AND dp_position IN (".$data['shape'].") )";
-					$cond = " OR ";
-				}
-				if($data['color'] != "")
-				{
-					$where .= $cond." ( dp_table_type = 2 AND dp_position IN (".$data['color'].") )";
-					$cond = " OR ";
-				}
-				if($data['size'] != "")
-				{
-					$where .= $cond." (dp_table_type = 43 AND dp_position IN (".$data['size'].") )";
-					$cond = " OR ";
-				}
-				if($data['clarity'] != "")
-				{
-					$where .= $cond." (dp_table_type = 3 AND dp_position IN (".$data['clarity'].") )";
-					$cond = " OR ";
-				}
-				if($data['discount'] != "")
-				{
-					$where .= $cond." (dp_table_type = 0 AND dp_position IN (".$data['discount'].") )";
-				}
-
-				 // echo $paramQuery . $where; 
-				$resources = $this->qry($paramQuery . $where,2);
-
-				$arrangeArray = array_column($resources, 'dp_parameters_code');
-				$imp = "'" . implode( "','", ($arrangeArray) ) . "'";
-				$stockDataQuery = "SELECT * FROM stock WHERE st_shape IN (".$imp.")  OR st_col IN (".$imp.") OR st_size = '".$data['size']."' OR st_cla IN (".$imp.") OR st_dis = '".$data['discount']."'  " ;
-				$stockResources = $this->qry($stockDataQuery,2);
-				// echo $this->qry($stockDataQuery,3);
-				$return_arr['flag'] = true;
-				$return_arr['data'] = $stockResources;
+				$return_arr = $this->getDiamondData($data);
 			}
 
 			return $return_arr;
 	 }
-	
+
+/*===========================================================
+					Function for get diamond parameter					
+===========================================================*/
+
+	public function getDiamondData($data)
+	{
+		$paramQuery = "SELECT dp_parameters_code FROM diamond_parameters WHERE ( 1 = 1 )";
+		$where = "";
+		$cond  = " AND ";
+		if($data['shape'] != "")
+		{
+			$where .= $cond." (dp_table_type = 0 AND dp_position IN (".$data['shape'].") )";
+			$cond = " OR ";
+		}
+		if($data['color'] != "")
+		{
+			$where .= $cond." ( dp_table_type = 2 AND dp_position IN (".$data['color'].") )";
+			$cond = " OR ";
+		}
+		if($data['size'] != "")
+		{
+			$where .= $cond." (dp_table_type = 43 AND dp_position IN (".$data['size'].") )";
+			$cond = " OR ";
+		}
+		if($data['clarity'] != "")
+		{
+			$where .= $cond." (dp_table_type = 3 AND dp_position IN (".$data['clarity'].") )";
+			$cond = " OR ";
+		}
+		if($data['discount'] != "")
+		{
+			$where .= $cond." (dp_table_type = 0 AND dp_position IN (".$data['discount'].") )";
+		}
+
+		$resources = $this->qry($paramQuery . $where,2);
+	 	$arrangeArray = array_column($resources, 'dp_parameters_code');
+		$imp = "'" . implode( "','", ($arrangeArray) ) . "'";
+		$stockDataQuery = "SELECT * FROM stock WHERE st_shape IN (".$imp.")  OR st_col IN (".$imp.") OR st_size = '".$data['size']."' OR st_cla IN (".$imp.") OR st_dis = '".$data['discount']."'  " ;
+		$stockResources = $this->qry($stockDataQuery,2);
+		// echo $this->qry($stockDataQuery,3);
+		$return_arr['flag'] = true;
+		$return_arr['data'] = !empty($stockResources) ? $stockResources : [];
+
+		return $return_arr;
+	}
 }
 ?>
